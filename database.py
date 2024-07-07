9ce1952faa92d04ba646d86f03ae077b8eb4049a
aroxan <aroxan.999@mail.ru>
2024-07-07 23:53:47 +0400

    Fix formatting and linting errors

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+asyncpg://aroxan:********@localhost/cookbook_"
-)
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://aroxan:********@localhost/cookbook_"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-AsyncSessionLocal = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, autoflush=False, expire_on_commit=False
 )
+
 Base = declarative_base()
 
 
