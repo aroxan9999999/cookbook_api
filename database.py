d35fe566cc1dfb16b02e19cd047275f23664da64
aroxan <aroxan.999@mail.ru>
2024-07-07 23:46:10 +0400

    Fix linting errors and update flake8 configuration

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://aroxan:********@localhost/cookbook_"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+asyncpg://aroxan:********@localhost/cookbook_"
+)
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
