97352224b542dbe1f3cc9074a9a2b4622c59116c
aroxan <aroxan.999@mail.ru>
2024-07-08 00:00:34 +0400

    Fix import sorting with isort

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://aroxan:********@localhost/cookbook_"
